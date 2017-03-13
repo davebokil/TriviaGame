d50dcbec81abf0d6b9fa612b1b218b6c2c63c9b0
David BOKIL <bokilmusic@Davids-Air.fios-router.home>
2017-03-13 19:20:15 -0400

    click fades

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,26 @@
 // Javascript function to wrap everything
-$( document ).ready(function() {
+$(document).ready(function() {
+
+    // Elements will fade into the page upon load
+    $(".pageheader").hide();
+    $("#subHeader").hide();
+    $("#instructions").hide();
+    $(".startbutton").hide();
+    $(".questionPanel").hide();
+    $(".pageheader").delay(1000).fadeIn(1000);
+    $("#subHeader").delay(3000).fadeIn(1000);
+    $("#instructions").delay(5000).fadeIn(1000);
+    $(".startbutton").delay(6000).fadeIn(1000);
+
+
+    // Clicking the start button hides the welcome screen 
+    $(".startbutton").on("click", function() {
+        $("#instructions").hide();
+        $(".startbutton").hide();
+        $(".questionPanel").delay(500).fadeIn(1000);
+
+    })
+
+
 
 });
